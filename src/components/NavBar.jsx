716e59fad760c510f9fc4d875f70d461c4381acf
Ana Badolato <badolatoana@gmail.com>
2024-10-18 00:45:10 +0200

    css navbar ongoing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,72 +1,83 @@
-import '../App.css'
-import "../CSS/navBar.css"
+import "../App.css";
+import "../CSS/navBar.css";
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 
 function NavBar() {
-
-  const navigate = useNavigate()
-  const {isLoggedIn, authenticateUser} = useContext(AuthContext)
+  const navigate = useNavigate();
+  const { isLoggedIn, authenticateUser } = useContext(AuthContext);
 
   const handleLogOut = async () => {
     try {
-      localStorage.removeItem("authToken")
-      await authenticateUser()
-      navigate("/")
+      localStorage.removeItem("authToken");
+      await authenticateUser();
+      navigate("/");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
     <nav className="navbar">
+      <div className="navbar-content">
+        <Link to="/" className="navbar-logo">
+          <p>Home</p>
+        </Link>
 
-    <Link to="/">
-    <p>Home</p>
-    </Link>
+        <p>Projects</p>
+        <p>Talents</p>
+        <p>Events</p>
 
-    <p>Projects</p>
-    <p>Talents</p>
-    <p>Events</p>
+        {!isLoggedIn && (
+          <Link to="/login">
+            <p>Log in</p>
+          </Link>
+        )}
 
-    {!isLoggedIn && <Link to="/login">
-    <p>Log in</p>
-    </Link>}
+        {!isLoggedIn && (
+          <Link to="/signup" className="button-large-yellow">
+            <p>Get Started</p>
+          </Link>
+        )}
+        <div className="navbar-private">
+          {isLoggedIn && (
+            <Link to="/profile" className="icon-text-element">
+              <img src="" alt="" />
+              <p>Profile</p>
+            </Link>
+          )}
 
-    {!isLoggedIn  && <Link to="/signup">
-    <p>Get Started</p>
-    </Link>}
+          {isLoggedIn && (
+            //!cambiar div por Link cuando tengamos las páginas
+            <div className="icon-text-element">
+              <img src="" alt="" />
+              <p>
+                <span>0</span>Notifications
+              </p>
+            </div>
+          )}
 
-    {isLoggedIn  && <Link to="/profile">
-    <div>
-    <img src="" alt="" />
-    <p>Profile</p>
-    </div>
-    </Link>}
+          {isLoggedIn && (
+            //!cambiar div por Link cuando tengamos las páginas
+            <div className="icon-text-element">
+              <img src="" alt="" />
+              <p>
+                <span>0</span>Messages
+              </p>
+            </div>
+          )}
 
-    {isLoggedIn  &&
-    <div>
-      <img src="" alt="" />
-      <p><span>0</span>Notifications</p>
-    </div>
-    }
-
-  {isLoggedIn  &&
-    <div>
-      <img src="" alt="" />
-      <p><span>0</span>Messages</p>
-    </div>
-  }
-
-
-    {isLoggedIn && <Link onClick={handleLogOut} to="/">
-    <p>Log out</p>
-    </Link>}
-
+          {isLoggedIn && (
+            <Link onClick={handleLogOut} to="/" className="icon-text-element">
+              <img src="" alt="" />
+              <p>Log out</p>
+            </Link>
+          )}
+        </div>
+      </div>
     </nav>
-    
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
