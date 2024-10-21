6a72ba4fc1db2f4432f741b0e5d0447bdb5868c8
Ana Badolato <badolatoana@gmail.com>
2024-10-21 21:52:50 +0200

    Co-authored-by: NuriaSoley <NuriaSoley@users.noreply.github.com>

--- src/pages/forms/EditProject.jsx
+++ src/pages/forms/EditProject.jsx
@@ -1,25 +1,28 @@
-import React, { useState, useContext, useEffect } from 'react'
+import "../../App.css"
+import "../../CSS/formGeneric.css"
+import "../../CSS/formProject.css"
+
+import React, { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../../context/auth.context";
-import { useNavigate, useParams } from "react-router-dom"; 
-import service from '../../services/config';
-import Autocomplete from "../../components/Autocomplete"
+import { useNavigate, useParams } from "react-router-dom";
+import service from "../../services/config";
+import Autocomplete from "../../components/Autocomplete";
 
 function EditProject() {
-
-  const params = useParams()
+  const params = useParams();
   const navigate = useNavigate();
 
   const { loggedUserId } = useContext(AuthContext);
 
   const [projectData, setProjectData] = useState({
     title: "",
-    mainObjective:"",
+    mainObjective: "",
     description: "",
     location: "",
-    image:"",
-    category:"",
-    teamMembers: []
-  })
+    image: "",
+    category: "",
+    teamMembers: [],
+  });
 
   const [isLoading, setIsLoading] = useState(true);
   const [uploadingImage, setUploadingImage] = useState(false);
@@ -28,22 +31,21 @@ function EditProject() {
   useEffect(() => {
     const fetchProjectData = async () => {
       try {
-        console.log("hola")
+        console.log("hola");
         console.log("Fetched project data:", params._id);
         const response = await service.get(`/project/${params.projectid}`);
         setProjectData({
           title: response.data.title || "",
-          mainObjective:response.data.mainObjective || "",
-          description: response.data.description || "", 
+          mainObjective: response.data.mainObjective || "",
+          description: response.data.description || "",
           location: response.data.location || "",
           image: response.data.image || "",
           category: response.data.category || "",
-          teamMembers:response.data.teamMembers || []
+          teamMembers: response.data.teamMembers || [],
         });
 
-
         // Pasar los miembros del equipo existentes a Autocomplete
-      updateTeamMembers(response.data.teamMembers || []);
+        updateTeamMembers(response.data.teamMembers || []);
         setIsLoading(false);
       } catch (error) {
         console.log("Error fetching project data:", error);
@@ -54,7 +56,7 @@ function EditProject() {
       fetchProjectData();
     }
   }, [loggedUserId]);
-  
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setProjectData((prevData) => ({
@@ -64,25 +66,25 @@ function EditProject() {
   };
 
   const handleImageUpload = async (e) => {
-    const file = e.target.files[0]; 
+    const file = e.target.files[0];
     if (!file) return;
 
-    const formData = new FormData(); 
+    const formData = new FormData();
     formData.append("file", file);
-    formData.append("upload_preset", "s3e3p4eg"); 
+    formData.append("upload_preset", "s3e3p4eg");
 
-    setUploadingImage(true); 
+    setUploadingImage(true);
     try {
-        const response = await axios.post(
+      const response = await axios.post(
         "https://api.cloudinary.com/v1_1/drqiultmd/image/upload",
         formData
       );
-      const imageUrl = response.data.secure_url; 
+      const imageUrl = response.data.secure_url;
       setProjectData((prevData) => ({
         ...prevData,
-        image: imageUrl, 
+        image: imageUrl,
       }));
-      setUploadingImage(false); 
+      setUploadingImage(false);
     } catch (error) {
       console.error("Error uploading image:", error);
       setUploadingImage(false);
@@ -94,7 +96,7 @@ function EditProject() {
 
     try {
       await service.put(`/project/${params.projectid}`, projectData);
-      setShowConfirmation(true); 
+      setShowConfirmation(true);
       setTimeout(() => {
         setShowConfirmation(false);
       }, 3000);
@@ -115,80 +117,125 @@ function EditProject() {
     }));
   };
 
-
-
   if (isLoading) {
     return <h3>Loading...</h3>;
   }
   return (
-    <div><form onSubmit={handleSubmit}>
-    <h3>New Project</h3>
     <div>
-      <label htmlFor="">Title: </label>
-      <input name="title" type="text" placeholder="Development of a Mobile App" value={projectData.title || ""} onChange={handleChange}/>
-    </div>
+      <form onSubmit={handleSubmit}>
+        <h3>New Project</h3>
+        <div>
+          <label htmlFor="title">Title: </label>
+          <input
+            name="title"
+            type="text"
+            placeholder="Development of a Mobile App"
+            value={projectData.title || ""}
+            onChange={handleChange}
+          />
+        </div>
 
-    <div>
-      <label htmlFor="">Main Objective</label>
-      <textarea name="mainObjective" type="text" maxLength={250} placeholder="Create an easy-to-use solution for financial management." value={projectData.mainObjective} onChange={handleChange}/>
-    </div>
+        <div>
+          <label htmlFor="mainObjective">Main Objective</label>
+          <textarea
+            name="mainObjective"
+            type="text"
+            maxLength={250}
+            placeholder="Create an easy-to-use solution for financial management."
+            value={projectData.mainObjective}
+            onChange={handleChange}
+          />
+        </div>
 
-    <div>
-      <label htmlFor="">Description</label>
-      <textarea name="description" maxLength={2000} placeholder="A mobile application that helps users manage their personal finances, allowing them to track their income and expenses." value={projectData.description || ""} onChange={handleChange}/>
-    </div>
+        <div>
+          <label htmlFor="description">Description</label>
+          <textarea
+            name="description"
+            maxLength={2000}
+            placeholder="A mobile application that helps users manage their personal finances, allowing them to track their income and expenses."
+            value={projectData.description || ""}
+            onChange={handleChange}
+          />
+        </div>
 
-    <div>
-      <label htmlFor="">Location</label>
-      <input name="location" type="text" placeholder="Barcelona, Spain" value={projectData.location || ""} onChange={handleChange} />
-    </div>
+        <div>
+          <label htmlFor="location">Location</label>
+          <input
+            name="location"
+            type="text"
+            placeholder="Barcelona, Spain"
+            value={projectData.location || ""}
+            onChange={handleChange}
+          />
+        </div>
 
-    <div>
-      {projectData.image && <img src={projectData.image} alt="Project" style={{ width: "200px" }} />}
-      <input name="image" type="file" onChange={handleImageUpload} />
-    </div>
-
-
-    <div>
-      <label htmlFor="">Category</label>
-      <select name="category" value={projectData.category || ""} onChange={handleChange}>
-        <option value="">Select a Category</option>
-        <option value="Technology & Innovation">Technology & Innovation</option>
-        <option value="Sustainability & Environment">Sustainability & Environment</option>
-        <option value="Art & Creativity">Art & Creativity</option>
-        <option value="Health & Wellness">Health & Wellness</option>
-        <option value="Education & Training">Education & Training</option>
-        <option value="Community & Social Impact">Community & Social Impact</option>
-      </select>
-    </div>
-
-     <div>
-      {<Autocomplete updateTeamMembers={updateTeamMembers} initialSelectedUsers={projectData.teamMembers}/>} 
-    </div>
-
-    <div className="button-container">
-            {/* Botón para guardar los cambios */}
-            <button type="submit" className="button-large-blue">
-              Save Changes
-            </button>
-            {/* Botón para volver al perfil sin guardar */}
-            <button
-              type="button"
-              className="button-large-grey"
-              onClick={handleGoToProfile}
-            >
-              Back to Profile
-            </button>
-          </div>
-
-          {/* Mensaje de confirmación al guardar los cambios */}
-          {showConfirmation && (
-            <div className="confirmation-message">
-              Project updated successfully!
-            </div>
+        <div>
+          {projectData.image && (
+            <img
+              src={projectData.image}
+              alt="Project"
+              style={{ width: "200px" }}
+            />
           )}
-  </form></div>
-  )
+          <input name="image" type="file" onChange={handleImageUpload} />
+        </div>
+
+        <div>
+          <label htmlFor="category">Category</label>
+          <select
+            name="category"
+            value={projectData.category || ""}
+            onChange={handleChange}
+          >
+            <option value="">Select a Category</option>
+            <option value="Technology & Innovation">
+              Technology & Innovation
+            </option>
+            <option value="Sustainability & Environment">
+              Sustainability & Environment
+            </option>
+            <option value="Art & Creativity">Art & Creativity</option>
+            <option value="Health & Wellness">Health & Wellness</option>
+            <option value="Education & Training">Education & Training</option>
+            <option value="Community & Social Impact">
+              Community & Social Impact
+            </option>
+          </select>
+        </div>
+
+        <div>
+          {
+            <Autocomplete
+              updateTeamMembers={updateTeamMembers}
+              initialSelectedUsers={projectData.teamMembers}
+            />
+          }
+        </div>
+
+        <div className="button-container">
+          {/* Botón para guardar los cambios */}
+          <button type="submit" className="button-large-blue">
+            Save Changes
+          </button>
+          {/* Botón para volver al perfil sin guardar */}
+          <button
+            type="button"
+            className="button-large-grey"
+            onClick={handleGoToProfile}
+          >
+            Back to Profile
+          </button>
+        </div>
+
+        {/* Mensaje de confirmación al guardar los cambios */}
+        {showConfirmation && (
+          <div className="confirmation-message">
+            Project updated successfully!
+          </div>
+        )}
+      </form>
+    </div>
+  );
 }
 
-export default EditProject
+export default EditProject;
