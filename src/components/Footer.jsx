0658fb52fa467a835a54d70cad85a121b3c0af3b
Núria Soley <nuria.soley.93@gmail.com>
2024-10-23 14:25:03 +0200

    footer almost ready

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
 import "../CSS/footer.css"
 import logoImg from "../assets/images/logoImg.svg";
+import githubIcon from "../assets/icons/githubIcon.svg"
+import linkedinicon from "../assets/icons/linkedinicon.svg"
+import xIcon from "../assets/icons/xIcon.svg"
+import tiktokIcon from "../assets/icons/tiktokIcon.svg"
+
 
 function Footer() {
   return (
@@ -8,11 +13,24 @@ function Footer() {
         <img src={logoImg} alt="Logo" />
 
         <div>
-          <p>About us??</p>
-          <p>About us??</p>
+          <p>Ana Badolato</p>
+          <img src={githubIcon} alt="github" style={{width: "20px"}} />
+          <img src={linkedinicon} alt="linkedin" style={{width: "20px"}}/>
         </div>
 
-        <p>Insta, FB, ...</p>
+        <div>
+          <p>Núria Soley</p>
+          <img src={githubIcon} alt="github" style={{width: "20px"}}/>
+          <img src={linkedinicon} alt="linkedin" style={{width: "20px"}}/>
+        </div>
+
+        <div>
+        <img src={xIcon} alt="github" style={{width: "20px"}}/>
+        <img src={tiktokIcon} alt="github" style={{width: "20px"}}/>
+        <img src={linkedinicon} alt="github" style={{width: "20px"}}/>
+        <img src={githubIcon} alt="github" style={{width: "20px"}}/>
+        </div>
+        
 
     </div>
   )
